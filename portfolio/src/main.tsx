f78b773feeb30cda0734e3dc75ff6e0019b2ef0a
김승섭 <sub9707@naver.com>
2025-01-19 06:14:09 +0900

    main - react import 오류 해결

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App'
+import React from 'react';  // React를 명시적으로 임포트
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
