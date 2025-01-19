d0cdbfd7975569b2440e0a0f9e2aaeb42c00255b
김승섭 <sub9707@naver.com>
2025-01-20 01:48:59 +0900

    footer 프레임 생성

--- portfolio/src/components/Footer/Footer.tsx
+++ portfolio/src/components/Footer/Footer.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import styles from './Footer.module.scss';
 
+import Antenna from '/src/assets/icons/antenna.svg';
 import Remote from '/src/assets/icons/remote.svg';
-import Git from '/src/assets/icons/git.svg';
+import Git from '/src/assets/icons/git2.svg';
+import Rotate from '/src/assets/icons/rotate.svg';
+import Cancel from '/src/assets/icons/cancel.svg';
+import Warn from '/src/assets/icons/warn.svg';
+import Signal from '/src/assets/icons/signal.svg';
+import Share from '/src/assets/icons/share.svg';
+import Alarm from '/src/assets/icons/alarm.svg';
+
 import FooterButton from './FooterButton';
 
 interface FooterProps {
@@ -15,10 +23,11 @@ const Footer: React.FC<FooterProps> = ({ theme }) => {
       <div className={styles.menuLeft}>
         <span><img src={Remote} alt='blue'/></span>
         <FooterButton icon={Git} text={'main*'} />
-        <FooterButton icon={Git} text={'0'} />
-        <FooterButton icon={Git} text={'0'} />
-        <FooterButton icon={Git} text={'0'} />
-        <FooterButton icon={Git} text={'Live Share'} />
+        <FooterButton icon={Rotate} />
+        <FooterButton icon={Cancel} text={'0'} />
+        <FooterButton icon={Warn} text={'0'} />
+        <FooterButton icon={Antenna} text={'0'} />
+        <FooterButton icon={Share} text={'Live Share'} />
       </div>
       <div className={styles.menuRight}>
       <FooterButton text={'줄 0, 열 0'} />
@@ -27,8 +36,8 @@ const Footer: React.FC<FooterProps> = ({ theme }) => {
       <FooterButton text={'CRLF'} />
       <FooterButton text={'{}'} />
       <FooterButton text={'TypeScript JSX'} />
-      <FooterButton icon={Git} text={'Go Live'} />
-      <FooterButton icon={Git} />
+      <FooterButton icon={Signal} text={'Go Live'} />
+      <FooterButton icon={Alarm} />
       </div>
     </div>
   );
