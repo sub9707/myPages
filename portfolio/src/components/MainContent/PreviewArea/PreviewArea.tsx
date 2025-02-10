15b26c56e1ea89ec96b84a41d2fdf62a3b7868b0
김승섭 <sub9707@naver.com>
2025-02-11 08:08:27 +0900

    수정

--- portfolio/src/components/MainContent/PreviewArea/PreviewArea.tsx
+++ portfolio/src/components/MainContent/PreviewArea/PreviewArea.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './PreviewArea.module.scss'
 import WindowHeader from '../Common/WindowHeader'
+import PreviewContent from './PreviewContent'
 
 const PreviewArea:React.FC = () => {
   return (
