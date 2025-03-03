de5f34b60aa749aaaa5faa403f3a3aa72c348550
김승섭 <sub9707@naver.com>
2025-03-04 00:35:26 +0900

    코드 에디터 - 파일 - 윈도우 탭 연동

--- portfolio/src/components/MainContent/CodeEditor/CodeEditorArea.tsx
+++ portfolio/src/components/MainContent/CodeEditor/CodeEditorArea.tsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import styles from './CodeEditorArea.module.scss'
-import CodeEditor from './CodeEditor'
-import WindowHeader from '../Common/WindowHeader'
+import React from 'react';
+import styles from './CodeEditorArea.module.scss';
+import CodeEditor from './CodeEditor';
+import WindowHeader from '../Common/WindowHeader';
+import { useFileContext } from '../../../context/FileContext';
 
 const CodeEditorArea: React.FC = () => {
+    const { editorFiles, previewFiles } = useFileContext();
 
     return (
         <div className={styles.editorWindow}>
             <div className={styles.windowTabs}>
-            <WindowHeader isPreview={false}/>
-            <WindowHeader isPreview={true}/>
+                {editorFiles.length > 0 && <WindowHeader isPreview={false} />}
+                {previewFiles.length > 0 && <WindowHeader isPreview={true} />}
             </div>
             <div className={styles.editorWrapper}>
                 <CodeEditor />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default CodeEditorArea
+export default CodeEditorArea;
